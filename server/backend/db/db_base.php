9d1a1deb3beecd709c2399e23d016f56e3460831
Matt Triviski <matt.triviski@gmail.com>
2015-08-14 22:12:01 -0700

    Commiting junk.  Doing something else so this crap will probably be removed at some point.

--- server/backend/db/db_base.php
+++ server/backend/db/db_base.php
@@ -5,12 +5,48 @@
 $database_host = "localhost";
 $database = "apollo";
 $db_port = "3306";
-$db_pwd = null;
-$db_user = "root";
 
-function get_connection() {
-	global $database, $database_host, $db_port, $db_pwd, $db_user;
+$query_user = "apollo_query";
+$query_pwd = "query_password";
 
+$insert_user = "apollo_insert";
+$insert_pwd = "sharonlicksballs6969";
+
+
+/**
+ * Connects to the db as the query user.
+ * 
+ * @return mysqli
+ */
+function get_query_connection() {
+	global $query_user, $query_pwd;
+	echo "query deal user" . $query_user . " pwd" . $query_pwd; 
+	return get_connection($query_user, $query_pwd);
+}
+
+/**
+ * Connects to the db as the query user.
+ * 
+ * @return mysqli
+ */
+function get_insert_connection() {
+	global $insert_user, $insert_pwd;
+	echo "insert deal user" . $insert_user . " pwd" . $insert_pwd;
+	
+	return get_connection($insert_user, $insert_pwd);
+}
+
+/**
+ * @param unknown $db_user
+ * @param unknown $db_pwd
+ * @return mysqli
+ */
+function get_connection($db_user, $db_pwd) {
+	global $database, $database_host, $db_port;
+
+	echo "connection deal user" . $db_user . " pwd " . $db_pwd . "<p>";
+	
+	
 	$connection = new mysqli($database_host, $db_user, $db_pwd, $database, $db_port);
 	
 	if ($connection->connect_error) {
--- server/index.php
+++ server/index.php
@@ -1,16 +1,17 @@
 <?php 
-require './backend/db/db_base.php';
+// require './backend/db/db_base.php';
+require 'backend/db/add_user.php';
 
-$database_host = "localhost";
-$database = "apollo";
-$db_port = "3306";
-$db_pwd = "";
-$db_user = "root";
+user_name_available("balls");
+// $con = get_connection("root", null);
+// $con->close();
 
-// $con = new mysqli($database_host, $db_user, null, $database, $db_port, $db_user);
-// $con = mysqli_connect($database_host);
-// $con = new mysqli($database_host, $db_user);
-$con = get_connection();
-$con->close();
+// $con = get_query_connection();
+// $con->close();
+
+// $con = get_insert_connection();
+// $con->close();
+
+
 echo "suck my balls on the index whore!";
 ?>
