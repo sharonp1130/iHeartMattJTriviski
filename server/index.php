3073427615093daf1b0ee998d2738723641765fe
Matt Triviski <matt.triviski@gmail.com>
2015-08-15 21:55:44 -0700

    removed junk and updated the index to show the php info for now.  Who cares.

--- server/index.php
+++ server/index.php
@@ -1,17 +1,3 @@
 <?php 
-// require './backend/db/db_base.php';
-require 'backend/db/add_user.php';
-
-user_name_available("balls");
-// $con = get_connection("root", null);
-// $con->close();
-
-// $con = get_query_connection();
-// $con->close();
-
-// $con = get_insert_connection();
-// $con->close();
-
-
-echo "suck my balls on the index whore!";
+phpinfo();
 ?>
