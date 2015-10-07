1221c720a4c4799bc343ee07c7918c53e706b30b
Matt Triviski <matt.triviski@gmail.com>
2015-10-06 21:07:34 -0700

    Removed stuff.  Leaving Zend

--- server/db/propel-bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Bootstrap php file to initialize the propel crap.
-//
-// setup the autoloading
-require_once 'vendor/autoload.php';
-
-// setup Propel
-require_once 'generated-conf/config.php';
-?>
--- sql/services.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: 
--- Date: 2015-08-02 22:54
-*/
-INSERT INTO `service` (`serviceId`,`description`) VALUES (1,'electrical');
-INSERT INTO `service` (`serviceId`,`description`) VALUES (2,'massage');
-INSERT INTO `service` (`serviceId`,`description`) VALUES (3,'plumbing');
-INSERT INTO `service` (`serviceId`,`description`) VALUES (4,'roofing');
-INSERT INTO `service` (`serviceId`,`description`) VALUES (5,'tickling');
