813cd4ba08454fbcbf20865e302cf05470f10b23
Matt Triviski <matt.triviski@gmail.com>
2015-11-26 15:39:21 -0800

    Initial commit when moving to java with spring and hibernate.

---
+++ src/main/java/help/me/HelpishApplication.java
@@ -0,0 +1,12 @@
+package help.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpishApplication.class, args);
+    }
+}
